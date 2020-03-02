14d9b02ec86c7c3e55d7cab3b45d8259192b0cf8
Sergii.Pivtorak <sepivtorak@deloitte.com>
2020-03-02 19:13:27 +0200

    fix

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -3,6 +3,7 @@ import { AppRegistry } from 'react-native'
 import App from 'components/src/App'
 
 AppRegistry.registerComponent('ReactMono', () => App)
+
 AppRegistry.runApplication('ReactMono', {
     rootTag: document.getElementById('root'),
 })
