c3c00fab9220d2354dda964220f10a82e22845be
Sergii.Pivtorak <sepivtorak@deloitte.com>
2020-04-06 16:03:35 +0300

    Updates

---
+++ precache-manifest.1fe2939f691bba9c79daec99be2a81ee.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8962d605fbe667eb50af6bad4cbcde",
+    "url": "/rn-monorepo/index.html"
+  },
+  {
+    "revision": "b922b6b0d7a1244940c4",
+    "url": "/rn-monorepo/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "b922b6b0d7a1244940c4",
+    "url": "/rn-monorepo/static/js/2.861394b3.chunk.js"
+  },
+  {
+    "revision": "5097d04867bc1a4fde27bd801060566d",
+    "url": "/rn-monorepo/static/js/2.861394b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a786260e958f28bc6e09",
+    "url": "/rn-monorepo/static/js/main.64a17713.chunk.js"
+  },
+  {
+    "revision": "c3b002cd7ff6aab5a385",
+    "url": "/rn-monorepo/static/js/runtime-main.20e50445.js"
+  },
+  {
+    "revision": "663e8c6bc0024c3f5a49f68e848fb460",
+    "url": "/rn-monorepo/static/media/BackIcon.663e8c6b.png"
+  },
+  {
+    "revision": "9911b9766091b25bcde680a82cd66314",
+    "url": "/rn-monorepo/static/media/LogoutIcon.9911b976.png"
+  },
+  {
+    "revision": "7df2d5bc8b1fce47c6f127850fde316a",
+    "url": "/rn-monorepo/static/media/chartjs-logo.7df2d5bc.png"
+  },
+  {
+    "revision": "47bf38eb0cd85394f06b5c0450f5cec8",
+    "url": "/rn-monorepo/static/media/donut-chart.47bf38eb.png"
+  },
+  {
+    "revision": "15554b015fffd56f6fb91f0f5e6a912a",
+    "url": "/rn-monorepo/static/media/icon-d3.15554b01.png"
+  },
+  {
+    "revision": "34014e32e6372ecbb482d7a8d201eade",
+    "url": "/rn-monorepo/static/media/icon-line-chart.34014e32.png"
+  },
+  {
+    "revision": "102ef17693ea9aad0cf0d728659cf4a3",
+    "url": "/rn-monorepo/static/media/icon_refresh.102ef176.png"
+  }
+]);
